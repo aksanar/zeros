69e8db0111ec80ef9498b83e3a9388a05c2e117c
aksanar <aksana.r@gmail.com>
2019-11-04 17:17:55 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+function getBaseLog(x, y) {
+  return Math.log(y) / Math.log(x);
+}
+
 module.exports = function zeros(expression) {
-  // your solution
+  const parts = expression.split('*');
+
+  let five = 0;
+  let two = 0;
+
+  parts.forEach((part) => {
+    const n = parseInt(part);  
+    if (part.includes('!!')) {  
+      for (let i = n; i > 0; i=i-2) {
+ 
+        if (i % 5 === 0) {
+          let myNumber = i;  
+          while (myNumber % 5 === 0) {
+            myNumber = myNumber / 5; 
+            five++
+          }
+
+        }
+        
+        if (i % 2 === 0) {
+          let myNumber = i;  
+          while (myNumber % 2 === 0) {
+            myNumber = myNumber / 2; 
+            two++
+          }
+        }
+      }      
+    } else {
+      for (let i = n; i > 0; i=i-1) {
+        let myNumber = i;  
+        while (myNumber % 5 === 0) {
+          myNumber = myNumber / 5; 
+          five++
+        }
+        if (i % 2 === 0) {
+          let myNumber = i;  
+          while (myNumber % 2 === 0) {
+            myNumber = myNumber / 2; 
+            two++
+          }
+        }
+      }
+    }  
+  })
+  return Math.min(two, five);
 }
